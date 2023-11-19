afa34a6fed824ff474320d80c0dd3f03c99744c9
Fiona Wang <usfiona@gmail.com>
2023-11-19 06:13:50 -0500

    main menu now opens the different guis. pizzamanagermain launches the main menu gui

--- src/main/java/com/example/rupizza/MainMenuController.java
+++ src/main/java/com/example/rupizza/MainMenuController.java
@@ -1,14 +1,83 @@
 package com.example.rupizza;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class MainMenuController {
     @FXML
-    private Label welcomeText;
+    private void openSpecialtyPizzaGUI(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("specialty-pizzas.fxml"));
+            Parent root = loader.load();
+
+            SpecialtyPizzaController controller = loader.getController();
+
+            Stage stage = new Stage();
+            stage.setTitle("Order Specialty Pizzas");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void openCustomizePizzaGUI(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("customize-pizza.fxml"));
+            Parent root = loader.load();
+
+            CustomizePizzaController controller = loader.getController();
+
+            Stage stage = new Stage();
+            stage.setTitle("Customize Your Pizza");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void openCurrentOrderGUI(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("current-order.fxml"));
+            Parent root = loader.load();
+
+            CurrentOrderController controller = loader.getController();
+
+            Stage stage = new Stage();
+            stage.setTitle("Order Details");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void openStoreOrdersGUI(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("store-orders.fxml"));
+            Parent root = loader.load();
+
+            StoreOrdersController controller = loader.getController();
+
+            Stage stage = new Stage();
+            stage.setTitle("Store Orders");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/example/rupizza/PizzaManagerMain.java
+++ src/main/java/com/example/rupizza/PizzaManagerMain.java
@@ -11,7 +11,7 @@ public class PizzaManagerMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PizzaManagerMain.class.getResource("main-menu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 450);
         stage.setTitle("RU Pizza <Main Menu>");
         stage.setScene(scene);
         stage.show();
